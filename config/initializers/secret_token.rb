04dceaf4f020ac982a924be96fc297f5898c067b
Johnny Waggener <john.waggener@gmail.com>
2011-11-21 18:49:37 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OonnUs::Application.config.secret_token = '98a19ad09223f2b8676246fe1fdc7fcc22282aacd0729290129fc063f2f9acc0ff7aacedeb58c5fe2640319c7fee5050b3d810ed7264894efb6d600a3a30bd62'
